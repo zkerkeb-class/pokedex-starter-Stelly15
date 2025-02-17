000a6d86e9bb694e92671464b46f70141b9dacf1
Stelly1506 <stelly.wu@edu.ece.fr>
2025-02-17 15:44:22 +0100

    avec le component searchBar mais on l'utilise pas

--- src/components/pokemonCard/index.jsx
+++ src/components/pokemonCard/index.jsx
@@ -15,30 +15,45 @@ const PokemonCard = ({ name, types, image, attack, defense, hp }) => {
 
   return (
     <span className="pokemon-card">
-      <div className="pokemon-name-container">
-        <h1 className="pokemon-name">{name}</h1>
-      </div>
-      <div className="image-container">
-      <img className="pokemon-image" src={image} alt={name} />
-      </div>
       <div className="pokemon-types-container">
         {types && types.map((type) => (
           <p key={type}>{type}</p>
         ))}
       </div>
-      <div className="pokemon-stats-container">
-        <p>Attack: {attack}</p>
-        <p>Defense: {defense}</p>
-        <p>HP: {currentHP}</p>
+      <div className="pokemon-name-container">
+        <h1 className="pokemon-name">{name}</h1>
       </div>
-      <button onClick={() => {
-        console.log("🚀 ~ bulbizard se mange une patate");
-        setCurrentHP(currentHP - 10);
-      }}>
-        Attaque
-      </button>
+      <div className="image-container">
+        <img className="pokemon-image" src={image} alt={name} />
+      </div>
+
+      <div className="pokemon-stats-container">
+        <div className="stat-row">
+          <p className="stat-label">Attack:</p>
+          <p className="stat-value">{attack}</p>
+        </div>
+        <div className="stat-row">
+          <p className="stat-label">Defense:</p>
+          <p className="stat-value">{defense}</p>
+        </div>
+        <div className="stat-row">
+          <p className="stat-label">HP:</p>
+          <p className="stat-value">{currentHP}</p>
+        </div>
+    </div>
+
+      
     </span>
   );
 };
 
+/*
+<div className="button-container">
+        <button onClick={() => {
+          console.log("🚀 ~ bulbizard se mange une patate");
+          setCurrentHP(currentHP - 10);
+        }}>
+          Attaque
+        </button>
+      </div>*/ 
 export default PokemonCard;
